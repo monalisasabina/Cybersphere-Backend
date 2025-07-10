2a170050a294c3c07714bbcab7015178296ddfc3
Monalisa Sabina <onyangomonalisa@hotmail.com>
2025-07-10 16:53:27 +0300

    added python syntax check to yml

--- server/models.py
+++ server/models.py
@@ -2,7 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 from datetime import datetime
 from flask_bcrypt import Bcrypt
-import random
 
 
 db = SQLAlchemy()
@@ -15,23 +14,23 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(50), nullable=False)
     lastname = db.Column(db.String(50), nullable=False)
-    username = db.Column(db.String(100), nullable= False, unique=True)
-    email = db.Column(db.String, nullable= False, unique=True)
-    role=db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    role = db.Column(db.String(50), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
-    _password_hash =  db.Column(db.String(100), nullable=False)
+    _password_hash = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
         return f"<User {self.username}>"
-    
+
     @property
     def password(self):
         raise AttributeError("Password is write-only")
-    
+
     @password.setter
-    def password(self,password):
+    def password(self, password):
         self._password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
-    
+
     def check_password(self, password):
         return bcrypt.check_password_hash(self._password_hash, password)
 
@@ -41,25 +40,24 @@ class Project(db.Model, SerializerMixin):
     __tablename__ = 'projects'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable= False)
-    description = db.Column(db.String, nullable= False)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String, nullable=False)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
-    
+
     # One to many relationship
     images = db.relationship('Image', back_populates='project',cascade='all, delete-orphan')
     # want images to be deleted when a project is deleted, set cascade
-    
+
     def __repr__(self):
         return f"<Projects {self.title}>"
-    
 
 
 class Image(db.Model, SerializerMixin):
     __tablename__ = 'images'
 
     id = db.Column(db.Integer, primary_key=True)
-    caption = db.Column(db.String(100), nullable= False)
-    url = db.Column(db.String, nullable= False)
+    caption = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String, nullable=False)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
 
     # To control images to be used in the gallery
@@ -81,9 +79,9 @@ class Downloads(db.Model, SerializerMixin):
     __tablename__ = 'downloads'
 
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(100), nullable= False)
+    filename = db.Column(db.String(100), nullable=False)
     file_url =db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String, nullable= False)
+    description = db.Column(db.String, nullable=False)
     uploaded_at = db.Column(db.DateTime, default=datetime.utcnow)
       
     def __repr__(self):
@@ -105,7 +103,7 @@ class BlogPost(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow)
     is_published = db.Column(db.Boolean, default=False)
-
+ 
     def __repr__(self):
         return f'<BlogPost {self.title}'
     
